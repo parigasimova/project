7ea130227446b18b3f0a0eb70a0a89089ab86689
parigasimova <periqasimova.454@gmail.com>
2023-09-16 13:11:54 +0400

    project

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
+const cors = require("cors");
 const express = require("express");
+const mysql = require("mysql2");
+
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => res.type('html').send(html));
+const connection = mysql.createConnection({
+  host: "b4tca61xzzqovujaasmp-mysql.services.clever-cloud.com",
+  user: "uhpat9wvruroat9b",
+  password: "xr5Y3rEFsj8iw41XbWhj",
+  database: "b4tca61xzzqovujaasmp",
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/users", (req, res) => {
+  connection.query("SELECT * FROM users", (err, results) => {
+    if (err) {
+      console.error("Kullanıcıları getirirken hata oluştu: " + err.stack);
+      res.status(500).send("Kullanıcıları getirirken hata oluştu.");
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.delete("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const query = "DELETE FROM users WHERE ID = ?";
+  connection.query(query, [userId], (err, results) => {
+    if (err) {
+      console.error("İstifadəçi silinərkən problem yarandı: " + err.stack);
+      res.status(500).send("Kullanıcı silinərkən xəta baş verdi.");
+    } else {
+      res.status(200).send("Kullanıcı başarılı bir şəkildə silindi.");
+    }
+  });
+});
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+app.post("/users", (req, res) => {
+  const { ad, soyad, password, adres } = req.body;
+  const query =
+    "INSERT INTO users (ad, soyad, password, adres) VALUES (?, ?, ?, ?)";
+
+  connection.query(query, [ad, soyad, password, adres], (err, results) => {
+    if (err) {
+      console.error(
+        "Yeni istifadəçi əlavə edilərkən problem yarandı: " + err.stack
+      );
+      res.status(500).send("Yeni istifadəçi əlavə edilərkən xəta baş verdi.");
+    } else {
+      res
+        .status(201)
+        .send("Yeni istifadəçi başarılı bir şəkildə əlavə edildi.");
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
